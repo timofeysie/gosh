bb136b29c904ede590c4553bf4afca05648b8c21
timofeysie <timofeyc@hotmail.com>
2019-09-15 02:42:57 +0000

    Work from the pi

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function createWindow () {
     }
   })
   win.loadFile('index.html')
-  win.webContents.openDevTools()
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
