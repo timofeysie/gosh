9ade1b6f25199a2def43593e0d7c4fcb1892314c
Tim <timofeyc@hotmail.com>
2019-09-16 09:22:29 +1000

    Made the vanilla-js window full screen

--- main.js
+++ main.js
@@ -12,7 +12,10 @@ function createWindow () {
     }
   })
   win.loadFile('index.html')
-  win.webContents.openDevTools()
+  win.maximize()
+  win.setFullScreen(true) 
+  win.setMenu(null)
+  //win.webContents.openDevTools()
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
